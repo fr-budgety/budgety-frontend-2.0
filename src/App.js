7ae33dc5fa8e2e88589db08c54b5f95219e127e7
frivolta <rivoltafilippo@gmail.com>
2019-09-09 19:27:08 +0200

    :sparkles: Adding login component to test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LoginPage from './pages/login/LoginPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
