bd77fd400ce6f31aaa8f79ebdb420c5e30bbedae
frivolta <rivoltafilippo@gmail.com>
2019-09-15 10:00:54 +0200

    :rotating_light: Add prettierrc configuration

--- src/components/sign-up-form/SignUpForm.jsx
+++ src/components/sign-up-form/SignUpForm.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+/** @format */
+
+import React, { Component } from 'react';
 
 class SignUpForm extends Component {
   render() {
@@ -8,20 +10,35 @@ class SignUpForm extends Component {
           <h1>Sign in</h1>
           <form>
             <div className="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              <label Htmlfor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                We will never share your email with anyone else.
+              </small>
             </div>
             <div className="form-group">
-    <label htmlFor="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Submit</button>
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
           </form>
         </div>
       </div>
-    )
+    );
   }
 }
 
